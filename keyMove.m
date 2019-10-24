6011e74f9aa17055100c01abe30dea69c8db1f22
awongkue <awongkue@EN4129420-209.fulton.ad.asu.edu>
2019-10-24 10:26:35 -0700

    Stuff

--- keyMove.m
+++ keyMove.m
@@ -5,29 +5,30 @@ while 1
         pause(0.1);
         switch key
             case 'uparrow'
-                brick.MoveMotor('AB', 50);
+                brick.MoveMotor('AB', 20);
                 pause(1);
                 disp('Up Arrow Pressed!');
                 
             case 'downarrow'
-                brick.MoveMotor('AB', -50);
+                brick.MoveMotor('AB', -20);
                 pause(1);
                 disp('Down arrow Pressed!');
                 
             case 'leftarrow'
-                brick.MoveMotor('A', -50);
-                brick.MoveMotor('B', 50);
+                brick.MoveMotor('A', -20);
+                brick.MoveMotor('B', 20);
                 pause(1);
                 disp('Left Arrow Pressed!');
                 
             case 'rightarrow'
-                brick.MoveMotor('B', -50);
-                brick.MoveMotor('A', 50);
+                brick.MoveMotor('B', -20);
+                brick.MoveMotor('A', 20);
                 pause(1);
                 disp('Right Arrow Pressed!');
                 
             case 0
                 disp('No Key Pressed!');
+                brick.StopMotor('AB', 'Coast');
             case 'q'
                 brick.StopAllMotors('Brake');
                 break;
